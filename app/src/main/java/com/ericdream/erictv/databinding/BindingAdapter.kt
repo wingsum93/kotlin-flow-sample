53c190225b2447e7661a217a6b0c0b9383542957
Eric Ho <wingsum.developer@gmail.com>
2021-08-16 02:29:12 +0800

    change coil library to load images

--- app/src/main/java/com/ericdream/erictv/databinding/BindingAdapter.kt
+++ app/src/main/java/com/ericdream/erictv/databinding/BindingAdapter.kt
@@ -2,38 +2,18 @@ package com.ericdream.erictv.databinding
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import coil.load
 import com.ericdream.erictv.R
-import com.ericdream.erictv.data.model.LiveChannel
-import com.ericdream.erictv.ui.home.ChannelAdapter
 
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(string: String?) {
-    string?.let {
-        Glide
-            .with(this)
-            .load(string)
-            .error(R.drawable.ic_baseline_help_24)
-            .into(this)
-
+    load(string) {
+        error(R.drawable.ic_baseline_help_24)
     }
-}
-
-@BindingAdapter("items")
-fun RecyclerView.setLiveChannels(list: List<LiveChannel>?) {
-    val adapter = this.adapter
-    if (adapter is ChannelAdapter) {
-        list?.let {
-            adapter.update(it)
-        }
-    }
-
 }
 
 @BindingAdapter("android:src")
 fun ImageView.setImageViewResource(int: Int) {
     this.setImageResource(int)
-
 }
